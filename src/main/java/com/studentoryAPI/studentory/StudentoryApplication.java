a2c3798cb9eb54ad5f5c57c2f51441a0caae2156
Pranav Mendiratta <pranavmendi@gmail.com>
2022-09-28 15:12:19 +0530

    Initial commit

---
+++ src/main/java/com/studentoryAPI/studentory/StudentoryApplication.java
@@ -0,0 +1,13 @@
+package com.studentoryAPI.studentory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentoryAPI/studentory/StudentoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentoryAPI.studentory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
